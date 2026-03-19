e3dd4e36138665863530007648994a58a28f28f2
aryansaraogi <aryansaraogi02@gmail.com>
2026-03-19 21:57:01 +0530

    addintion

--- components/portfolio/projects.tsx
+++ components/portfolio/projects.tsx
@@ -61,7 +61,7 @@ const otherProjects = [
   {
     title: "EasyML",
     description:
-      "A platform that has a collection of pre-trained machine learning models for various tasks like Spam Detection .",
+      "A platform that has a collection of pre-trained machine learning models for various tasks like Spam Detection etc.",
     technologies: ["TypeScript", "React","Machine Learning", "Email.js" , "Tailwind CSS"],
     github: "https://github.com",
     demo: "https://example.com",
